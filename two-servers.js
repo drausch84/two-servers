dfe8fbaa98a2d2dfc6b121479675b1b337b7b4ed
David Rausch <David@Davids-MacBook-Pro-2.local>
2018-05-30 11:05:46 -0600

    new commit

--- two-servers.js
+++ two-servers.js
@@ -2,22 +2,30 @@
 var http = require("http");
 
 // Define a port to listen for incoming requests
-var PORT = 7000;
-
+var PORT1 = 7000;
+var PORT2 = 7500;
 // Create a generic function to handle requests and responses
-function handleRequest(request, response) {
+function handleRequest1(request, response) {
 
   // Send the below string to the client when the user visits the PORT URL
-  response.end("It Works!! You're so smart and sexy, GIVE ME A HUG!!!!!! " + request.url);
+  response.end("It Works!! You're so smart and sexy, GIVE ME A HUG!!!!!! ");
 }
 
+function handleRequest2(request, response){
+    response.end("I smell a copy-and-paste job you lazy jackass!");
+}
 // Use the Node HTTP package to create our server.
 // Pass the handleRequest function to empower it with functionality.
-var server = http.createServer(handleRequest);
+var server1 = http.createServer(handleRequest1);
+var server2 = http.createServer(handleRequest2);
 
 // Start our server so that it can begin listening to client requests.
-server.listen(PORT, function() {
+server.listen(PORT1, function() {
 
   // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+  console.log("Server listening on: http://localhost:" + PORT1);
+});
+
+server.listen(PORT2, function(){
+    console.log("Server listening on: http://localhost:" + PORT2);
 });
